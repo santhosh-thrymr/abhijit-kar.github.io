12ad2298ba5f2e5b87ff13ba9f1a09ba481ab534
Abhijit Kar <ak24284@gmail.com>
2018-02-18 16:35:31 +0530

    Border around mat card added, plus a sample included in about

--- src/app/features/about/about.module.ts
+++ src/app/features/about/about.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import {
-  AboutRoutingModule,
-  AboutComponents
-} from "./about-routing.module";
+  MatCardModule,
+  MatListModule
+} from "@angular/material";
+
+import { AboutRoutingModule, AboutComponents } from "./about-routing.module";
+
+const MaterialModules = [
+  MatCardModule,
+  MatListModule
+];
 
 @NgModule({
-  imports: [CommonModule, AboutRoutingModule],
+  imports: [CommonModule, AboutRoutingModule, ...MaterialModules],
   declarations: AboutComponents
 })
 export class AboutModule {}
