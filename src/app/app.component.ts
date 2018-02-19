8eb9d38ea3bd924da8c0b282c339d0a9a7e310ce
Abhijit Kar <ak24284@gmail.com>
2018-02-20 00:46:32 +0530

    Almost fixed the scroll problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, ViewChild, ElementRef } from "@angular/core";
 import { Router, NavigationEnd } from "@angular/router";
 import { GoogleAnalyticsService } from "./shared";
+
+import { MatSidenav } from "@angular/material";
+
+// import {} from "@angular/cdk";
 
 declare var ga;
 
@@ -10,6 +14,16 @@ declare var ga;
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  @ViewChild("sideNav") sideNav: MatSidenav;
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll(event: Event) {
+    if (this.sideNav && this.sideNav.opened) {
+      console.log(event.target);
+      this.sideNav.close();
+    }
+  }
+
   constructor(
     public router: Router,
     private googleAnalyticsService: GoogleAnalyticsService
