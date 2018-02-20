869987f0c7f4987ce35ecef79b8938d685f0304a
Abhijit Kar <ak24284@gmail.com>
2018-02-20 22:15:41 +0530

    Portfolio added as well to Projects

--- src/app/shared/pipes/highlighter.pipe.ts
+++ src/app/shared/pipes/highlighter.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: "highlighter" })
 export class HighlighterPipe implements PipeTransform {
   colors: string[] = [
-    "#F44336", // RED
+    "#F44336", // Red
     "#FF5722", // Deep Orange
     "#FF9800", // Orange
     "#FFC107", // Amber
@@ -12,7 +12,7 @@ export class HighlighterPipe implements PipeTransform {
     "#E6EE9C", // Lime
     "#AED581", // Lighter Green
     "#8BC34A", // Light Green
-    "#4CAF50" // Green
+    "#4CAF50"  // Green
   ];
   transform(percentage: number): string {
     return this.colors[Math.floor(percentage / 10)];
