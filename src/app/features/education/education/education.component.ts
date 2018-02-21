184935a73edea3b2c225b0f5b3ece6d3265ecfb7
Abhijit Kar <ak24284@gmail.com>
2018-02-21 23:32:02 +0530

    Education also moved under tab

--- src/app/features/education/education/education.component.ts
+++ src/app/features/education/education/education.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "ak-education",
   templateUrl: "education.component.html",
   styleUrls: ["education.component.css"]
 })
-export class EducationComponent {}
+export class EducationComponent {
+  educations: Observable<any> = this.httpClient.get("./assets/configs/educations.json");
+
+  constructor(private httpClient: HttpClient) {}
+}
