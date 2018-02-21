82a4b4bdda822c103db25787c694d54bf18b76b3
Abhijit Kar <ak24284@gmail.com>
2018-02-21 23:38:23 +0530

    Faq moved to its own tab

--- src/app/features/faq/faq/faq.component.ts
+++ src/app/features/faq/faq/faq.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "ak-faq",
   templateUrl: "faq.component.html",
   styleUrls: ["faq.component.css"]
 })
-export class FaqComponent {}
+export class FaqComponent {
+  faqs: Observable<any> = this.httpClient.get("./assets/configs/faqs.json");
+
+  constructor(private httpClient: HttpClient) {}
+}
