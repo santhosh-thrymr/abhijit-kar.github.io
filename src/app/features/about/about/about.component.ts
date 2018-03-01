b93cc207115511127dad9356ae2ba49adeaa3b0a
Abhijit Kar <ak24284@gmail.com>
2018-03-01 16:29:39 +0530

    About page completed with Contributions links

--- src/app/features/about/about/about.component.ts
+++ src/app/features/about/about/about.component.ts
@@ -1,8 +1,21 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+
+import { LinkHandlerService } from "../../../shared";
 
 @Component({
   selector: "ak-about",
   templateUrl: "about.component.html",
   styleUrls: ["about.component.css"]
 })
-export class AboutComponent {}
+export class AboutComponent {
+  contributions: Observable<any> = this.httpClient.get(
+    "./assets/configs/contributions.json"
+  );
+
+  constructor(
+    private httpClient: HttpClient,
+    public linkHandlerService: LinkHandlerService
+  ) {}
+}
