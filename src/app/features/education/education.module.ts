99198ce600c12319313c724e1d22bb50d4a09373
Abhijit Kar <ak24284@gmail.com>
2018-03-04 13:13:12 +0530

    Education updated

--- src/app/features/education/education.module.ts
+++ src/app/features/education/education.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
 import { MatCardModule, MatButtonModule } from "@angular/material";
 
+import { SharedModule } from "../../shared/shared.module";
 import {
   EducationRoutingModule,
   EducationComponents
@@ -11,7 +11,12 @@ import {
 const MaterialModules = [MatCardModule, MatButtonModule];
 
 @NgModule({
-  imports: [CommonModule, EducationRoutingModule, ...MaterialModules],
+  imports: [
+    CommonModule,
+    EducationRoutingModule,
+    SharedModule,
+    ...MaterialModules
+  ],
   declarations: EducationComponents
 })
 export class EducationModule {}
