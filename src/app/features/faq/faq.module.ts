13820023e5be1cb235d1e5155b02e6702f61cfc6
Abhijit Kar <ak24284@gmail.com>
2018-03-04 12:42:04 +0530

    Summarizer introduced to FAQ

--- src/app/features/faq/faq.module.ts
+++ src/app/features/faq/faq.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
 import { MatCardModule, MatListModule } from "@angular/material";
 
+import { SharedModule } from "../../shared/shared.module";
 import { FaqRoutingModule, FaqComponents } from "./faq-routing.module";
 
 const MaterialModules = [MatCardModule, MatListModule];
 
 @NgModule({
-  imports: [CommonModule, FaqRoutingModule, ...MaterialModules],
+  imports: [CommonModule, FaqRoutingModule, SharedModule, ...MaterialModules],
   declarations: FaqComponents
 })
 export class FaqModule {}
