a15c56bf9a10a52da20d03a0e39de6ae5e457e56
Abhijit Kar <ak24284@gmail.com>
2018-03-04 13:15:45 +0530

    Blog files removed

--- src/app/features/blog/blog/blog.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "ak-blog",
-  templateUrl: "blog.component.html",
-  styleUrls: ["blog.component.css"]
-})
-export class BlogComponent {}
