1507d2f05eae5f072ca6322579b98a20aa486ae6
Abhijit Kar <ak24284@gmail.com>
2018-03-05 21:54:01 +0530

    Removed Chat Link

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,8 @@
 import { Component } from "@angular/core";
-import { LinkHandlerService } from "../shared";
 
 @Component({
   selector: "ak-footer",
   templateUrl: "footer.component.html",
   styleUrls: ["footer.component.css"]
 })
-export class FooterComponent {
-  chatBot = {
-    name: "Chat Bot",
-    category: "Chat Bot",
-    url: "https://console.dialogflow.com/api-client/demo/embedded/abhijit-kar"
-  };
-
-  constructor(public linkHandlerService: LinkHandlerService) {}
-}
+export class FooterComponent {}
